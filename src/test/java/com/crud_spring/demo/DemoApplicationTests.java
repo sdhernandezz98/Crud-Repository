e7dc0a36725f44c759311536d5cfadf810aa3ea0
Wilson Hz <Wilson Hz@WilsonHernandez>
2024-09-18 00:39:29 -0500

    Creo el proyecto, e inicializo spring boot

---
+++ src/test/java/com/crud_spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
